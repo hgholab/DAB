8978d6b3bdd76962669b6201725711ee04f399f2
Hossein Ghollabdouz <hgholab@gmail.com>
2026-02-19 18:36:29 +0200

    First commit. Configured the clock tree of the board.

---
+++ CPU1_RAM/subdir_vars.mk
@@ -0,0 +1,15 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../2837xD_RAM_lnk_cpu1.cmd 
+
+LIB_SRCS += \
+C:/ti/C2000Ware_6_00_01_00/libraries/calibration/hrpwm/f2837xd/lib/SFO_v8_fpu_lib_build_c28_driverlib.lib \
+C:/ti/C2000Ware_6_00_01_00/driverlib/f2837xd/driverlib/ccs/Debug/driverlib.lib 
+
+
---
+++ src/main.c
@@ -0,0 +1,305 @@
+#include <stdint.h>
+
+#include "device.h"
+#include "driverlib.h"
+
+clock_init(void)
+{
+        // Configure system clock to 190 MHz.
+        uint32_t device_setclock_cfg = (SYSCTL_OSCSRC_XTAL | SYSCTL_IMULT(38) | SYSCTL_FMULT_NONE |
+                                        SYSCTL_SYSDIV(2) | SYSCTL_PLL_ENABLE);
+        SysCtl_setClock(device_setclock_cfg);
+
+        // Configure XCLKOUT so that it reads CPU system clock with a 1 to 8 scale.
+        SysCtl_selectClockOutSource(SYSCTL_CLOCKOUT_SYSCLK);
+        SysCtl_setXClk(SYSCTL_XCLKOUT_DIV_8);
+
+        // Connect GPIO73 to mux channel 3 which is XCLKOUT for GPIO73.
+        GPIO_setPinConfig(GPIO_73_XCLKOUT);
+        GPIO_setPadConfig(73U, GPIO_PIN_TYPE_STD);
+
+        // Set GPIO15 to show OUTPUTXBAR4 signal.
+        GPIO_setPinConfig(GPIO_15_OUTPUTXBAR4);
+        GPIO_setPadConfig(15U, GPIO_PIN_TYPE_STD);
+        GPIO_setDirectionMode(15U, GPIO_DIR_MODE_OUT);
+
+        // Set XBAR so that XCLKOUT can be seen on GPIO15.
+        XBAR_setInputPin(XBAR_INPUT6, 73U);
+        XBAR_setOutputMuxConfig(XBAR_OUTPUT4, XBAR_OUT_MUX11_INPUTXBAR6);
+        XBAR_enableOutputMux(XBAR_OUTPUT4, XBAR_MUX11);
+}
+
+int main(void)
+{
+        clock_init();
+
+        for (;;)
+                ;
+}
+
+/*
+//
+// Defines
+//
+#define RESULTS_BUFFER_SIZE 256
+#define EX_ADC_RESOLUTION   12
+// 12 for 12-bit conversion resolution, which supports (ADC_MODE_SINGLE_ENDED)
+// Sample on single pin (VREFLO is the low reference)
+// Or 16 for 16-bit conversion resolution, which supports (ADC_MODE_DIFFERENTIAL)
+// Sample on pair of pins (difference between pins is converted, subject to
+// common mode voltage requirements; see the device data manual)
+
+//
+// Globals
+//
+uint16_t adcAResults[RESULTS_BUFFER_SIZE]; // Buffer for results
+uint16_t index;                            // Index into result buffer
+volatile uint16_t bufferFull;              // Flag to indicate buffer is full
+
+//
+// Function Prototypes
+//
+void initADC(void);
+void initEPWM(void);
+void initADCSOC(void);
+__interrupt void adcA1ISR(void);
+
+//
+// Main
+//
+void main(void)
+{
+        //
+        // Initialize device clock and peripherals
+        //
+        Device_init();
+
+        //
+        // Disable pin locks and enable internal pullups.
+        //
+        Device_initGPIO();
+
+        //
+        // Initialize PIE and clear PIE registers. Disables CPU interrupts.
+        //
+        Interrupt_initModule();
+
+        //
+        // Initialize the PIE vector table with pointers to the shell Interrupt
+        // Service Routines (ISR).
+        //
+        Interrupt_initVectorTable();
+
+        //
+        // Interrupts that are used in this example are re-mapped to ISR functions
+        // found within this file.
+        //
+        Interrupt_register(INT_ADCA1, &adcA1ISR);
+
+        //
+        // Set up the ADC and the ePWM and initialize the SOC
+        //
+        initADC();
+        initEPWM();
+        initADCSOC();
+
+        //
+        // Initialize results buffer
+        //
+        for (index = 0; index < RESULTS_BUFFER_SIZE; index++)
+        {
+                adcAResults[index] = 0;
+        }
+
+        index      = 0;
+        bufferFull = 0;
+
+        //
+        // Enable ADC interrupt
+        //
+        Interrupt_enable(INT_ADCA1);
+
+        //
+        // Enable Global Interrupt (INTM) and realtime interrupt (DBGM)
+        //
+        EINT;
+        ERTM;
+
+        //
+        // Loop indefinitely
+        //
+        while (1)
+        {
+                //
+                // Start ePWM1, enabling SOCA and putting the counter in up-count mode
+                //
+                EPWM_enableADCTrigger(EPWM1_BASE, EPWM_SOC_A);
+                EPWM_setTimeBaseCounterMode(EPWM1_BASE, EPWM_COUNTER_MODE_UP);
+
+                //
+                // Wait while ePWM1 causes ADC conversions which then cause interrupts.
+                // When the results buffer is filled, the bufferFull flag will be set.
+                //
+                while (bufferFull == 0)
+                {
+                }
+                bufferFull = 0; // Clear the buffer full flag
+
+                //
+                // Stop ePWM1, disabling SOCA and freezing the counter
+                //
+                EPWM_disableADCTrigger(EPWM1_BASE, EPWM_SOC_A);
+                EPWM_setTimeBaseCounterMode(EPWM1_BASE, EPWM_COUNTER_MODE_STOP_FREEZE);
+
+                //
+                // Software breakpoint. At this point, conversion results are stored in
+                // adcAResults.
+                //
+                // Hit run again to get updated conversions.
+                //
+                ESTOP0;
+        }
+}
+
+//
+// Function to configure and power up ADCA.
+//
+void initADC(void)
+{
+
+        //
+        // Set ADCDLK divider to /4
+        //
+        ADC_setPrescaler(ADCA_BASE, ADC_CLK_DIV_4_0);
+
+        //
+        // Set resolution and signal mode (see #defines above) and load
+        // corresponding trims.
+        //
+#if (EX_ADC_RESOLUTION == 12)
+        ADC_setMode(ADCA_BASE, ADC_RESOLUTION_12BIT, ADC_MODE_SINGLE_ENDED);
+#elif (EX_ADC_RESOLUTION == 16)
+        ADC_setMode(ADCA_BASE, ADC_RESOLUTION_16BIT, ADC_MODE_DIFFERENTIAL);
+#endif
+
+        //
+        // Set pulse positions to late
+        //
+        ADC_setInterruptPulseMode(ADCA_BASE, ADC_PULSE_END_OF_CONV);
+
+        //
+        // Power up the ADC and then delay for 1 ms
+        //
+        ADC_enableConverter(ADCA_BASE);
+        DEVICE_DELAY_US(1000);
+}
+
+//
+// Function to configure ePWM1 to generate the SOC.
+//
+void initEPWM(void)
+{
+        //
+        // Disable SOCA
+        //
+        EPWM_disableADCTrigger(EPWM1_BASE, EPWM_SOC_A);
+
+        //
+        // Configure the SOC to occur on the first up-count event
+        //
+        EPWM_setADCTriggerSource(EPWM1_BASE, EPWM_SOC_A, EPWM_SOC_TBCTR_U_CMPA);
+        EPWM_setADCTriggerEventPrescale(EPWM1_BASE, EPWM_SOC_A, 1);
+
+        //
+        // Set the compare A value to 1000 and the period to 1999
+        // Assuming ePWM clock is 100MHz, this would give 50kHz sampling
+        // 50MHz ePWM clock would give 25kHz sampling, etc.
+        // The sample rate can also be modulated by changing the ePWM period
+        // directly (ensure that the compare A value is less than the period).
+        //
+        EPWM_setCounterCompareValue(EPWM1_BASE, EPWM_COUNTER_COMPARE_A, 1000);
+        EPWM_setTimeBasePeriod(EPWM1_BASE, 1999);
+
+        //
+        // Set the local ePWM module clock divider to /1
+        //
+        EPWM_setClockPrescaler(EPWM1_BASE, EPWM_CLOCK_DIVIDER_1, EPWM_HSCLOCK_DIVIDER_1);
+
+        //
+        // Freeze the counter
+        //
+        EPWM_setTimeBaseCounterMode(EPWM1_BASE, EPWM_COUNTER_MODE_STOP_FREEZE);
+}
+
+//
+// Function to configure ADCA's SOC0 to be triggered by ePWM1.
+//
+void initADCSOC(void)
+{
+        //
+        // Configure SOC0 of ADCA to convert pin A0. The EPWM1SOCA signal will be
+        // the trigger.
+        // - For 12-bit resolution, a sampling window of 15 (75 ns at a 200MHz
+        //   SYSCLK rate) will be used.  For 16-bit resolution, a sampling window
+        //   of 64 (320 ns at a 200MHz SYSCLK rate) will be used.
+        // - NOTE: A longer sampling window will be required if the ADC driving
+        //   source is less than ideal (an ideal source would be a high bandwidth
+        //   op-amp with a small series resistance). See TI application report
+        //   SPRACT6 for guidance on ADC driver design.
+        //
+
+#if (EX_ADC_RESOLUTION == 12)
+        ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN0, 15);
+#elif (EX_ADC_RESOLUTION == 16)
+        ADC_setupSOC(ADCA_BASE, ADC_SOC_NUMBER0, ADC_TRIGGER_EPWM1_SOCA, ADC_CH_ADCIN0, 64);
+#endif
+
+        //
+        // Set SOC0 to set the interrupt 1 flag. Enable the interrupt and make
+        // sure its flag is cleared.
+        //
+        ADC_setInterruptSource(ADCA_BASE, ADC_INT_NUMBER1, ADC_SOC_NUMBER0);
+        ADC_enableInterrupt(ADCA_BASE, ADC_INT_NUMBER1);
+        ADC_clearInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1);
+}
+
+//
+// ADC A Interrupt 1 ISR
+//
+__interrupt void adcA1ISR(void)
+{
+        //
+        // Add the latest result to the buffer
+        //
+        adcAResults[index++] = ADC_readResult(ADCARESULT_BASE, ADC_SOC_NUMBER0);
+
+        //
+        // Set the bufferFull flag if the buffer is full
+        //
+        if (RESULTS_BUFFER_SIZE <= index)
+        {
+                index      = 0;
+                bufferFull = 1;
+        }
+
+        //
+        // Clear the interrupt flag
+        //
+        ADC_clearInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1);
+
+        //
+        // Check if overflow has occurred
+        //
+        if (true == ADC_getInterruptOverflowStatus(ADCA_BASE, ADC_INT_NUMBER1))
+        {
+                ADC_clearInterruptOverflowStatus(ADCA_BASE, ADC_INT_NUMBER1);
+                ADC_clearInterruptStatus(ADCA_BASE, ADC_INT_NUMBER1);
+        }
+
+        //
+        // Acknowledge the interrupt
+        //
+        Interrupt_clearACKGroup(INTERRUPT_ACK_GROUP1);
+}
+
+*/
